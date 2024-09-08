405c58a9b40205e9c6dab57889b09262cf6e4242
Safkat Sharif <safkatsadli@gmail.com>
2024-09-09 01:13:39 +0600

    Initial Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from './components/reuseableComponents/MaxWidthWrapper'
+
+const App = () => {
+  return (
+    <MaxWidthWrapper>
+      <div>hello</div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default App
